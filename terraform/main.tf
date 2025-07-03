cd05b6352b52f7c0e540cf34c23d3b172cea8fe4
AnithaCuba <melodyanita57@gmail.com>
2025-07-03 11:10:57 -0500

    acloud2

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ resource "aws_s3_bucket" "output" {
 
 # Rol de ejecución para Lambda
 resource "aws_iam_role" "lambda_exec_role" {
-  name = "lambda_exec_role"
+  name = "lambda_exec_role_test01"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
